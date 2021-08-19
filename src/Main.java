ed649417dc10e97a9df071cd5d1ff482ff7ed5cf
unknown <lbvbxlbvbx@gmail.com>
2021-08-19 19:20:11 +0300

    addeted Inorder traversal

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         TreeNode root = new TreeNode();
         TreeNode.defaultGeneration(root);
         TreeNode root1 = new TreeNode(55);
         TreeNode.defaultGeneration(root1);
+        List<Integer> inOrderList = new LinkedList<>();
+        //   for (Integer integer : inOrderList = TreeNode.inorderTraversal(root1)) {
+        //       System.out.println(integer);
+        //    }
+        for (Integer integer : inOrderList = TreeNode.inorderTraversal(root)) {
+            System.out.println(integer);
+        }
     }
 }
